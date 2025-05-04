8784f75eb41cbdc96bba17c63af90ac0a405ab9e
KaiyouS <kserra@addu.edu.ph>
2025-05-05 02:35:46 +0800

    feat: Add calendar feature and back-to-top button
    - Implemented a dynamic calendar page using calendar-data.json.
    - Added a programmable search engine.
    - Added corresponding HTML, CSS, and JavaScript for the calendar.
    - Introduced a back-to-top button with its icon.
    - Updated styles for contact, header, main, and program offerings pages.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,32 @@ scrollToTopBtn.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const searchIcon = document.getElementById('search-icon');
+    const searchOverlay = document.getElementById('search-overlay');
+    const closeBtn = document.getElementById("search-close-btn");
+    
+    if (searchIcon && searchOverlay) {
+        searchIcon.addEventListener('click', (event) => {
+            event.preventDefault();
+            searchOverlay.classList.add('active');
+            document.body.classList.add('overlay-active');
+        });
+
+        searchOverlay.addEventListener('click', (event) => {
+            if (event.target === searchOverlay) {
+                searchOverlay.classList.remove('active');
+                document.body.classList.remove('overlay-active'); 
+            }
+        });
+        
+        closeBtn.addEventListener('click', (event) => {
+            if (event.target === closeBtn) {
+                searchOverlay.classList.remove('active');
+                document.body.classList.remove('overlay-active'); 
+            }
+        });
+    }
+});
